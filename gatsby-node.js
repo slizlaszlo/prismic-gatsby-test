3bdd21d6c5b6d99b4804422040e58dc09b1cf7ed
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-09-03 23:00:00 +0200

    build: update gatsby configs

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,11 @@
  */
 
 // You can delete this file if you're not using it
+var fs = require('fs');
+var dir = './.cache/caches/@prismicio/gatsby-source-prismic-graphql'
+
+exports.onPreBootstrap = () => {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+}
