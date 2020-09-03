84f65dc80db07593af4af6ebdb253401691aba96
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-09-03 23:00:41 +0200

    feat: update page template

---
+++ src/templates/page.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { RichText } from 'prismic-reactjs';
+
+import Layout from '../components/layout';
+
+export const query = graphql`
+query PageQuery($id: String) {
+  prismic {
+    allPages(id: $id) {
+      edges {
+        node {
+          content
+          page_title
+          _meta {
+            id
+            uid
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
+const Page = (props) => {
+
+  const pageTitle = props.data.prismic.allPages.edges[0].node.page_title;
+  const content = props.data.prismic.allPages.edges[0].node.content;
+
+  return (
+    <Layout>
+      <RichText render={pageTitle} />
+      <RichText render={content} />
+    </Layout>
+  )
+}
+
+export default Page;
